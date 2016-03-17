88fe4aaffd961bd35583745d7af766faa94ad43d
Ahmad Shamli <shamli@ahmadshamli.com>
2016-03-17 17:43:46 +0800

    first commit

---
+++ analyze.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+#Fail2Ban Log Analyzer
+#This script will read,analyze and output to a file for better readable format
+#This script is based from  http://www.the-art-of-web.com/system/fail2ban-log/
+#
+#
+#
+#
+
+OUTPUTLOCATION='/var/www/1ma/'
+FILENAME='fail2ban.html'
+WHOISSERVER='http://whois.ens.my/'
+OUTPUTFILE=$OUTPUTLOCATION$FILENAME
+LOG=TRUE
+
+###HEAD###
+cat > $OUTPUTFILE <<END
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Fail2Ban Log Analyzer</title>
+</head>
+<body>
+<div id="body">
+<div id="title">
+<h1>Fail2ban Log Analyzer</h1>
+</div>
+END
+###/HEAD###
+
+###BODY###
+echo '<div id="row"><h2>Banned Count by IP < today ></h2><table>' >> $OUTPUTFILE
+#grep "Ban " /var/log/fail2ban.log | grep `date +%Y-%m-%d` | awk '{print $NF}' | sort | uniq -c | sort -n > TEMPVAR
+grep "Ban " /var/log/fail2ban.log | grep `date +%Y-%m-%d` | awk -F[\ \:] '{print $10,$8}' | sort | uniq -c | sort -n > TEMPVAR
+I=0
+cat TEMPVAR | while IFS= read -r line
+do
+	if [ $I -eq 0 ]
+	then
+		echo "<tr>" >> $OUTPUTFILE
+	fi
+	IP=$( printf '%s\n' "$line" | awk '{print $2}' )
+	IPCOUNT=$( printf '%s\n' "$line" | awk '{print $1}' )
+	IPTYPE=$( printf '%s\n' "$line" | awk '{print $3}' )
+	echo "<td><a href=\"$WHOISSERVER$IP\">"$IPCOUNT"&nbsp;"$IP"&nbsp;"$IPTYPE"</a><td>" >> $OUTPUTFILE
+	if [ $I -eq 5 ]
+	then
+		echo  "</tr>" >> $OUTPUTFILE
+		I=0
+	elif [ $I -lt 5 ]
+	then
+		I=$(( I + 1 ))
+	fi
+done
+echo '</table></div>' >> $OUTPUTFILE
+echo '<div id="row"><h2>Banned Count Group by Date and section</h2><table>' >> $OUTPUTFILE
+zgrep -h "Ban " /var/log/fail2ban.log* | awk '{print $5,$1}' | sort | uniq -c | sort -n > TEMPVAR
+I=0
+cat TEMPVAR | while IFS= read -r line
+do
+	if [ $I -eq 0 ]
+	then
+		echo "<tr>" >> $OUTPUTFILE
+	fi
+	echo "<td>"$line"</a><td>" >> $OUTPUTFILE
+	if [ $I -eq 5 ]
+	then
+		echo  "</tr>" >> $OUTPUTFILE
+		I=0
+	elif [ $I -lt 5 ]
+	then
+		I=$(( I + 1 ))
+	fi
+done
+echo '</table></div>' >> $OUTPUTFILE
+echo '<div id="row"><h2>Banned Count by IP</h2><table>' >> $OUTPUTFILE
+#zgrep -h "Ban " /var/log/fail2ban.log* | awk '{print $NF}' | sort | uniq -c | sort > TEMPVAR
+zgrep -h "Ban " /var/log/fail2ban.log* | awk -F[\ \:] '{print $10,$8}' | sort | uniq -c | sort -n > TEMPVAR
+I=0
+cat TEMPVAR | while IFS= read -r line
+do
+	if [ $I -eq 0 ]
+	then
+		echo "<tr>" >> $OUTPUTFILE
+	fi
+	IP=$( printf '%s\n' "$line" | awk '{print $2}' )
+	IPCOUNT=$( printf '%s\n' "$line" | awk '{print $1}' )
+	IPTYPE=$( printf '%s\n' "$line" | awk '{print $3}' )
+	echo "<td><a href=\"$WHOISSERVER$IP\">"$IPCOUNT"&nbsp;"$IP"&nbsp;"$IPTYPE"</a><td>" >> $OUTPUTFILE
+	if [ $I -eq 5 ]
+	then
+		echo  "</tr>" >> $OUTPUTFILE
+		I=0
+	elif [ $I -lt 5 ]
+	then
+		I=$(( I + 1 ))
+	fi
+done
+echo '</table></div>' >> $OUTPUTFILE
+echo '<div id="row"><h2>Banned Count by Subnet</h2><table>' >> $OUTPUTFILE
+zgrep -h "Ban " /var/log/fail2ban.log* | awk '{print $NF}' | awk -F\. '{print $1"."$2"."}' | sort | uniq -c  | sort -n | tail > TEMPVAR
+I=0
+cat TEMPVAR | while IFS= read -r line
+do
+	if [ $I -eq 0 ]
+	then
+		echo "<tr>" >> $OUTPUTFILE
+	fi
+	echo "<td>"$line"<td>" >> $OUTPUTFILE
+	if [ $I -eq 5 ]
+	then
+		echo  "</tr>" >> $OUTPUTFILE
+		I=0
+	elif [ $I -lt 5 ]
+	then
+		I=$(( I + 1 ))
+	fi
+done
+echo '</table></div>' >> $OUTPUTFILE
+echo '<div id="row"><h2>Banned Count with Hostname</h2><table>' >> $OUTPUTFILE
+#awk '($(NF-1) = /Ban/){print $NF,"("$NF")"}' /var/log/fail2ban.log | sort | logresolve | uniq -c | sort -n > TEMPVAR
+zgrep -h "Ban " /var/log/fail2ban.log* | awk '{print $NF}' | sort | logresolve | sort | uniq -c | sort -n > TEMPVAR
+I=0
+cat TEMPVAR | while IFS= read -r line
+do
+	if [ $I -eq 0 ]
+	then
+		echo "<tr>" >> $OUTPUTFILE
+	fi
+	echo "<td>"$line"<td>" >> $OUTPUTFILE
+	if [ $I -eq 1 ]
+	then
+		echo  "</tr>" >> $OUTPUTFILE
+		I=0
+	elif [ $I -lt 1 ]
+	then
+		I=$(( I + 1 ))
+	fi
+done
+echo '</table></div>' >> $OUTPUTFILE
+###/BODY###
+
+
+###Footer###
+cat >> $OUTPUTFILE<<END
+</div>
+</body>
+</html>
+END
+###/Footer###
